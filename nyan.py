c792700ada11045aaa93b6708f9bd54be25630f6
Jay Kamat <jaygkamat@gmail.com>
2018-03-04 18:42:38 -0500

    Add first version of qutenyan

---
+++ nyan.py
@@ -0,0 +1,68 @@
+
+
+from PyQt5.QtCore import pyqtSlot, QSize
+from PyQt5.QtWidgets import QSlider
+from PyQt5 import QtCore
+
+from qutebrowser.mainwindow.statusbar import percentage
+
+NYAN_MAX_WIDTH=150
+
+class NyanPercentage(QSlider):
+
+	"""Reading percentage displayed in the statusbar."""
+
+	def __init__(self, parent=None):
+		"""Constructor. Set percentage to 0%."""
+		super().__init__(parent)
+		self.set_perc(0, 0)
+		self.raw = False
+		self.setOrientation(QtCore.Qt.Horizontal)
+		self.setStyleSheet("""
+			QSlider::groove {
+				border: 1px none;
+				height: 16px;
+				margin: 0 12px;
+			}
+
+			QSlider::sub-page:horizontal {
+				border-image: url(./img/rainbow.svg);
+			}
+
+			QSlider::handle:horizontal {
+				image: url(./img/nyan.svg);
+				width: 30px;
+				margin: -24px -12px;
+			}
+
+			QTSlider::sub-page:horizontal {
+				background: #0000FF;
+			}
+        """)
+		self.setMaximumWidth(150)
+
+	@pyqtSlot(int, int)
+	def set_perc(self, x, y):  # pylint: disable=unused-argument
+		"""Setter to be used as a Qt slot.
+
+		Args:
+			x: The x percentage (int), currently ignored.
+			y: The y percentage (int)
+		"""
+		if y is None:
+			# We don't know, the pos, set to 0
+			self.setValue(0)
+		else:
+			self.setValue(y)
+
+	def on_tab_changed(self, tab):
+		"""Update scroll position when tab changed."""
+		self.set_perc(*tab.scroller.pos_perc())
+
+
+def init_nyan():
+	# This is really bad, but we need nyans
+	percentage.Percentage = NyanPercentage
+
+
+init_nyan()
